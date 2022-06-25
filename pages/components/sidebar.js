69f7a0a0630b2a074dbc39ce30744fdf3c0a3e06
benjamin-Keller <zhero240@gmail.com>
2022-06-26 01:01:36 +0200

    Removes ml-1

--- pages/components/sidebar.js
+++ pages/components/sidebar.js
@@ -17,16 +17,16 @@ export default function SideBar() {
                 <div className="display-f flex-direction-column pb-2">
                     <h4>Layout</h4>
                     <Link href="/docs/layout/breakpoints" passHref>
-                        <a className={router.pathname == "/docs/layout/breakpoints" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Breakpoints <span className="badge-secondary text-white ml-1">WIP</span></a>
+                        <a className={router.pathname == "/docs/layout/breakpoints" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Breakpoints <span className="badge-secondary text-white">WIP</span></a>
                     </Link>
                     <Link href="/docs/layout/grid" passHref>
-                        <a className={router.pathname == "/docs/layout/grid" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Grid <span className="badge-secondary text-white ml-1">WIP</span></a>
+                        <a className={router.pathname == "/docs/layout/grid" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Grid <span className="badge-secondary text-white">WIP</span></a>
                     </Link>
                 </div>
                 <div className="display-f flex-direction-column">
                     <h4>Forms</h4>
                     <Link href="/docs/forms/input_fields" passHref>
-                        <a className={router.pathname == "/docs/forms/input_fields" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Input Fields <span className="badge-secondary text-white ml-1">WIP</span></a>
+                        <a className={router.pathname == "/docs/forms/input_fields" ? "text-secondary text-hover-secondary" : "text-hover-secondary"}>Input Fields <span className="badge-secondary text-white">WIP</span></a>
                     </Link>
                 </div>
             </div>
